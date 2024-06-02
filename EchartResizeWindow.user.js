0012c745d9b76bf489ecbbaf53b4fc2959b35c90
maywoodmedical <115953271+maywoodmedical@users.noreply.github.com>
2024-06-02 16:53:52 -0700

    Update EchartResizeWindow.user.js

--- EchartResizeWindow.user.js
+++ EchartResizeWindow.user.js
@@ -6,10 +6,12 @@
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @updateURL 
+// @downloadURL 
 // @grant    GM_addStyle
 // @grant    GM.getValue
 // @noframes
-// @version 1.0
+// @version 1.3
 // ==/UserScript==
 //- The @grant directives are needed to restore the proper sandbox.
 /* global $, waitForKeyElements */
@@ -22,8 +24,11 @@
     function maximizeWindow() {
         var screenWidth = window.screen.availWidth;
         var newWidth = screenWidth * 0.75; // 75% of screen width
-
+        // Calculate left position to start from the left of the screen
+        var leftPosition = screen.left;
         window.resizeTo(newWidth, window.screen.availHeight);
+        // Move window to the left of the screen
+        window.moveTo(leftPosition, 0);
     }
 
     // Wait for the document to be fully loaded
