8d3b0913f3c75668ed93ba58fda9fd4532d32fac
maywoodmedical <115953271+maywoodmedical@users.noreply.github.com>
2024-06-02 16:49:22 -0700

    Update EchartHideStuff.user.js

--- EchartHideStuff.user.js
+++ EchartHideStuff.user.js
@@ -1,20 +1,26 @@
 // ==UserScript==
-// @name        Echart Hide Stuff
+// @name        Hide Echart Stuff
 // @namespace   https://github.com/maywoodmedical/Oscar
-// @description Hides the Research bar, Right Navigation Bar headers, etc. 
+// @description Hides the Research bar
 // @include     */casemgmt/forward.jsp?action=view&demographic*
+// @include     */provider/providercontrol.jsp?*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @updateURL https://github.com/maywoodmedical/Oscar/raw/main/EchartHideStuff.user.js
+// @downloadURL https://github.com/maywoodmedical/Oscar/raw/main/EchartHideStuff.user.js
 // @grant    GM_addStyle
 // @grant    GM.getValue
 // @noframes
-// @version 1.0
+// @version 1.3
 // ==/UserScript==
 //- The @grant directives are needed to restore the proper sandbox.
 /* global $, waitForKeyElements */
 
+
+
 /*
+//removes the Research toolbar 
 waitForKeyElements ("legend:contains('Research')", removeFieldsetWithLegend);
 
 function removeFieldsetWithLegend (jNode) {
@@ -30,12 +36,36 @@ function removeFieldsetWithLegend (jNode) {
     }
 }
 */
-    
+
+
+//removes the OpenOSP logo next to Patient Name
+waitForKeyElements ("#branding-logo", removeBrandingLogoDiv);
+
+function removeBrandingLogoDiv (jNode) {
+    'use strict';
+    jNode.remove();
+}
+
+
+//removes the Cortico logo on Labs header
+waitForKeyElements("a[href='https://help.cortico.ca/help/oscar-lab-auto-labelling?version=latest']", removeElement);
+
+function removeElement(jNode) {
+    'use strict';
+    jNode.remove();
+}
+waitForKeyElements("span.tw-bg-cortico-blue", removeElement);
+
+function removeElement(jNode) {
+    'use strict';
+    jNode.remove();
+}
+
+//removes multiple headers in Navigation Bars
 waitForKeyElements ("#unresolvedIssues", removeUnresolvedIssuesDiv);
 
 function removeUnresolvedIssuesDiv (jNode) {
     'use strict';
-    // Remove the unresolved issues div element
     jNode.remove();
 }
 
@@ -43,7 +73,6 @@ waitForKeyElements ("#resolvedIssues", removeResolvedIssuesDiv);
 
 function removeResolvedIssuesDiv (jNode) {
     'use strict';
-    // Remove the resolved issues div element
     jNode.remove();
 }
 
@@ -51,7 +80,6 @@ waitForKeyElements ("#Guidelines", removeGuidelinesDiv);
 
 function removeGuidelinesDiv (jNode) {
     'use strict';
-    // Remove the guidelines div element
     jNode.remove();
 }
 
@@ -59,20 +87,18 @@ waitForKeyElements ("#contacts", removeContactsDiv);
 
 function removeContactsDiv (jNode) {
     'use strict';
-    // Remove the contacts div element
     jNode.remove();
 }
-
 
 waitForKeyElements ("#msgs", removeMsgsDiv);
 
 function removeMsgsDiv (jNode) {
     'use strict';
-    // Remove the messenger div element
     jNode.remove();
 }
 
 
+//removes multiple buttons at bottom of Echart
 waitForKeyElements ("button:contains('Display Resolved Issues')", hideDisplayResolvedButton);
 
 function hideDisplayResolvedButton (jNode) {
@@ -80,7 +106,6 @@ function hideDisplayResolvedButton (jNode) {
     // Hide the button with text "Display Resolved Issues"
     jNode.hide();
 }
-
 
 waitForKeyElements ("button:contains('Display Unresolved Issues')", hideDisplayUnresolvedButton);
 
@@ -90,20 +115,40 @@ function hideDisplayUnresolvedButton (jNode) {
     jNode.hide();
 }
 
+waitForKeyElements ("#assignIssueSection", removeassignIssueSectionDiv);
 
-// Function to remove the last two characters of text (BC) inside the HIN/PHN element
-function removeLastTwoCharacters(jNode) {
-    // Get the HTML content of the element
-    var htmlContent = jNode.html();
-
-    // Remove the last two characters
-    var modifiedHtml = htmlContent.slice(0, -2);
-
-    // Set the modified HTML content back to the element
-    jNode.html(modifiedHtml);
+function removeassignIssueSectionDiv (jNode) {
+    'use strict';
+    // Remove the assign issue button
+    jNode.remove();
 }
 
-// Wait for the element with id "patient-hin" to be available, then execute the function
-waitForKeyElements("#patient-hin", removeLastTwoCharacters);
+
+//removes the last two characters 'BC' at the end of the PHN in header
+// Get the element with id "patient-hin"
+var hinElement = document.getElementById('patient-hin');
+// Get the text node within the element
+var textNode = hinElement.childNodes[1];
+// Get the text content of the text node
+var textContent = textNode.textContent;
+// Check if the last two characters are 'BC'
+if (textContent.slice(-2) === 'BC') {
+    // Remove the last two characters
+    var modifiedText = textContent.slice(0, -2);
+    
+    // Set the modified text content back to the text node
+    textNode.textContent = modifiedText;
+}
 
 
+(function() {
+    'use strict';
+    
+    // Define custom CSS to adjust image position
+    GM_addStyle(`
+        img[src^='data:image/png;base64'] {
+            position: absolute !important;
+            top: 0px !important;
+        }
+    `);
+})();
