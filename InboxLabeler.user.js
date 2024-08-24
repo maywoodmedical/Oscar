88321f49164f0309ba1fd81693ad13aecea4e6e9
maywoodmedical <115953271+maywoodmedical@users.noreply.github.com>
2024-08-24 12:01:29 -0700

    Update InboxLabeler.user.js

--- InboxLabeler.user.js
+++ InboxLabeler.user.js
@@ -4,8 +4,8 @@
 // @description Copy and paste prelabel to document label field
 // @include     *oscar/lab/CA/ALL/labDisplay.jsp?inWindow=true&segmentID=*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
-// @updateURL 
-// @downloadURL 
+// @updateURL https://github.com/maywoodmedical/Oscar/blob/main/InboxLabeler.user.js
+// @downloadURL https://github.com/maywoodmedical/Oscar/blob/main/InboxLabeler.user.js
 // @version 1.0
 // @grant       none
 // ==/UserScript==
