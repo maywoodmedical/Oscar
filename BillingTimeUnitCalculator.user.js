fba7fac5795f065cbcbc309948f2ee9ae7b68cf9
maywoodmedical <115953271+maywoodmedical@users.noreply.github.com>
2024-11-03 15:56:31 -0800

    Update BillingTimeUnitCalculator.user.js

--- BillingTimeUnitCalculator.user.js
+++ BillingTimeUnitCalculator.user.js
@@ -7,8 +7,8 @@
 // @include     */oscar/billing/CA/BC/SaveBilling.do*
 // @include     */oscar/CaseManagementEntry.do*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
-// @updateURL 
-// @downloadURL 
+// @updateURL https://github.com/maywoodmedical/Oscar/raw/main/BillingTimeUnitCalculator.user.js
+// @downloadURL https://github.com/maywoodmedical/Oscar/raw/main/BillingTimeUnitCalculator.user.js
 // @version 1.1
 // @grant       none
 // ==/UserScript==
