2ebb59f8903ed6f4801d2599a821b0dcd92fc88a
maywoodmedical <115953271+maywoodmedical@users.noreply.github.com>
2025-05-19 23:26:07 -0700

    Create ConsultAutoloadHistory.user.js

---
+++ ConsultAutoloadHistory.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name        Consult Autoload History
+// @namespace   https://github.com/maywoodmedical/Oscar
+// @description Automatically click buttons to load patient history and medications
+// @include  *oscarEncounter/oscarConsultationRequest/ConsultationFormRequest.jsp?*
+// @require   http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js
+// @updateURL 
+// @downloadURL 
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Wait for the page to fully load before executing
+    window.addEventListener('load', function() {
+        // List of button IDs to click
+        const buttonIds = [
+            "Concerns_clinicalInformation", 
+            "MedHistory_clinicalInformation", 
+            "SocHistory_concurrentProblems", 
+            "FamHistory_concurrentProblems", 
+            "fetchMedications_currentMedications", 
+            "OMeds_currentMedications",
+            "fetchAllergies_allergies"
+        ];
+
+        // Loop through the button IDs and click each one
+        buttonIds.forEach(function(buttonId) {
+            const button = document.getElementById(buttonId);
+            if (button) {
+                button.click();
+                console.log(`Button with id "${buttonId}" clicked.`);
+            } else {
+                console.log(`Button with id "${buttonId}" not found.`);
+            }
+        });
+    });
+})();
