e8d7433042ea8143d10a80834ce22dbc8ac29e0d
maywoodmedical <115953271+maywoodmedical@users.noreply.github.com>
2025-05-19 23:20:45 -0700

    Create MedicationsPanel.user.js

---
+++ MedicationsPanel.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name        Medications Panel
+// @namespace   https://github.com/maywoodmedical/Oscar
+// @description move Stage Medication button to left of containing box
+// @include     *oscarRx/choosePatient.do?*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
+// @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @updateURL 
+// @downloadURL 
+// @grant    GM_addStyle
+// @grant    GM.getValue
+// @noframes
+// @version 1.0
+// ==/UserScript==
+//- The @grant directives are needed to restore the proper sandbox.
+/* global $, waitForKeyElements */
+
+
+(function() {
+    'use strict';
+
+    window.addEventListener('load', function() {
+        const stageButton = document.querySelector('input[type="button"][name="stage"][value="Stage Medication"]');
+        const cancelButton = document.querySelector('input[type="button"][name="cancel"][value="Cancel"]');
+
+        if (stageButton && cancelButton && stageButton.parentNode === cancelButton.parentNode) {
+            const container = stageButton.parentNode;
+
+            if (container.parentNode) {
+                // Create a wrapper div to hold the buttons side-by-side
+                const buttonWrapper = document.createElement('div');
+                buttonWrapper.style.display = 'inline-flex';
+                buttonWrapper.style.gap = '8px'; // space between buttons
+                buttonWrapper.style.marginRight = '10px';
+                buttonWrapper.style.verticalAlign = 'top';
+
+                // Move both buttons into the wrapper
+                buttonWrapper.appendChild(stageButton);
+                buttonWrapper.appendChild(cancelButton);
+
+                // Insert the wrapper before the original container
+                container.parentNode.insertBefore(buttonWrapper, container);
+            }
+        }
+    }, false);
+})();
