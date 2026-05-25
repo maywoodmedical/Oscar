d47cbb958041e3a8109d0abb906f513c72a596be
maywoodmedical <115953271+maywoodmedical@users.noreply.github.com>
2026-05-25 14:59:59 -0700

    Update EchartResizeWindow.user.js

--- EchartResizeWindow.user.js
+++ EchartResizeWindow.user.js
@@ -1,21 +1,19 @@
 // ==UserScript==
-// @name        EchartResizeWindow
-// @namespace   https://github.com/maywoodmedical/Oscar
-// @description Resizes the Echart window
-// @include     */casemgmt/forward.jsp?action=view&demographic*
-// @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
-// @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @updateURL https://github.com/maywoodmedical/Oscar/raw/main/EchartResizeWindow.user.js
-// @downloadURL https://github.com/maywoodmedical/Oscar/raw/main/EchartResizeWindow.user.js
-// @grant    GM_addStyle
-// @grant    GM.getValue
+// @name         EchartResizeWindow
+// @namespace    https://github.com/maywoodmedical/Oscar
+// @description  Resizes the Echart window and waits for the case note textarea to load before focusing.
+// @include      */casemgmt/forward.jsp?action=view&demographic*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @updateURL    https://github.com/maywoodmedical/Oscar/raw/main/EchartResizeWindow.user.js
+// @downloadURL  https://github.com/maywoodmedical/Oscar/raw/main/EchartResizeWindow.user.js
+// @grant        GM_addStyle
+// @grant        GM.getValue
 // @noframes
-// @version 1.4
+// @version      1.6
 // ==/UserScript==
 //- The @grant directives are needed to restore the proper sandbox.
 /* global $, waitForKeyElements */
-
 
 (function() {
     'use strict';
@@ -24,16 +22,32 @@
     function maximizeWindow() {
         var screenWidth = window.screen.availWidth;
         var newWidth = screenWidth * 0.75; // 75% of screen width
-        // Calculate left position to start from the left of the screen
-        var leftPosition = screen.left;
+        var leftPosition = window.screen.left || 0;
         window.resizeTo(newWidth, window.screen.availHeight);
-        // Move window to the left of the screen
         window.moveTo(leftPosition, 0);
     }
 
-    // Wait for the document to be fully loaded
+    // Function to handle the textbox once waitForKeyElements finds it
+    function handleTextArea(jNode) {
+        // jNode is already the jQuery object for '#caseNote_note0'
+        var strLength = jNode.val().length;
+        
+        jNode.focus();
+        
+        // Put cursor at the end
+        jNode[0].setSelectionRange(strLength, strLength);
+        
+        // Backup trick to force cursor to the end
+        var val = jNode.val();
+        jNode.val('').val(val);
+    }
+
+    // Maximize window immediately on document ready
     $(document).ready(function() {
-        // Maximize window
         maximizeWindow();
     });
+
+    // Dynamically watch the DOM and wait for the textarea to load
+    waitForKeyElements("#caseNote_note0", handleTextArea);
+
 })();
